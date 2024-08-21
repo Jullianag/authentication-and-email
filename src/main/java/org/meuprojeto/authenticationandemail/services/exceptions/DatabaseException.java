460cc25d0c7cea6856d95d9eb9906f40b659b7a3
Jullianag <juh_g@hotmail.com>
2024-08-21 17:02:35 -0300

    UserControler: findAll, findById, Insert, update, delete

--- src/main/java/org/meuprojeto/authenticationandemail/services/exceptions/DatabaseException.java
+++ src/main/java/org/meuprojeto/authenticationandemail/services/exceptions/DatabaseException.java
@@ -1,7 +1,7 @@
 package org.meuprojeto.authenticationandemail.services.exceptions;
 
 public class DatabaseException extends RuntimeException {
-    
+
     public DatabaseException(String message) {
         super(message);
     }
